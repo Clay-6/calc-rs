a2940d88f0eaefb42f137b5078b4e92555e3f366
Clayman <claytonwambu@gmail.com>
2022-03-27 00:02:07 +0000

    Tidied up match statement over the args

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,28 @@ fn main() {
     let args = Args::parse();
 
     match args.operation.as_str() {
-        "add" | "+" => {
-            println!("{}", Arithmetic::Add(args.a, args.b));
-        }
-        "sub" | "-" => {
-            println!("{}", Arithmetic::Subtract(args.a, args.b));
-        }
-        "mult" | "*" => {
-            println!("{}", Arithmetic::Multiply(args.a, args.b));
-        }
-        "div" | "/" => {
-            println!("{}", Arithmetic::Divide(args.a, args.b));
-        }
-        "quot" => {
-            println!("{}", Arithmetic::Quotient(args.a, args.b));
-        }
-        "mod" | "%" => {
-            println!("{}", Arithmetic::Modulo(args.a, args.b));
-        }
-        "pow" | "^" => {
-            println!("{}", Arithmetic::Pow(args.a, Some(args.b as i64)));
-        }
-        "sqrt" => {
-            println!("{}", Arithmetic::Sqrt(args.a, Some(args.b as u32)));
-        }
-        "fac" | "!" => {
-            println!("{}", Arithmetic::Factorial(args.a as i64));
-        }
-        "fib-upto" => {
-            Fibonacci::UpTo(args.a as u64);
-        }
-        "fib-oflen" => {
-            Fibonacci::OfLength(args.a as u64);
-        }
+        "add" | "+" => println!("{}", Arithmetic::Add(args.a, args.b)),
+
+        "sub" | "-" => println!("{}", Arithmetic::Subtract(args.a, args.b)),
+
+        "mult" | "*" => println!("{}", Arithmetic::Multiply(args.a, args.b)),
+
+        "div" | "/" => println!("{}", Arithmetic::Divide(args.a, args.b)),
+
+        "quot" => println!("{}", Arithmetic::Quotient(args.a, args.b)),
+
+        "mod" | "%" => println!("{}", Arithmetic::Modulo(args.a, args.b)),
+
+        "pow" | "^" => println!("{}", Arithmetic::Pow(args.a, Some(args.b as i64))),
+
+        "sqrt" => println!("{}", Arithmetic::Sqrt(args.a, Some(args.b as u32))),
+
+        "fac" | "!" => println!("{}", Arithmetic::Factorial(args.a as i64)),
+
+        "fib-upto" => Fibonacci::UpTo(args.a as u64),
+
+        "fib-oflen" => Fibonacci::OfLength(args.a as u64),
+
         cmd => eprintln!("Invalid command \"{}\" Use -h or --help for help", cmd),
     }
 }
